574b8536a1bfa8053096fa33b1f01a194b0b43dc
PAB1610 <pabloanast1@gmail.com>
2023-03-21 15:55:52 -0300

    Utilizando equals() rashCode()

---
+++ src/com/projeto/equals_hashCode/MainCarro.java
@@ -0,0 +1,23 @@
+package com.projeto.equals_hashCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainCarro {
+    public static void main(String[] args) {
+        List<Carro> listaCarros = new ArrayList<>();
+
+        listaCarros.add(new Carro("ford"));
+        listaCarros.add(new Carro("Chevrolet"));
+        listaCarros.add(new Carro("Volkswagen"));
+
+        System.out.println(listaCarros.contains(new Carro("ford")));
+        System.out.println((new Carro("ford").hashCode()));
+        System.out.println((new Carro("ford1").hashCode()));
+
+        Carro carro1 = new Carro("ford");
+        Carro carro2 = new Carro("Chevrolet");
+
+        System.out.println(carro1.equals(carro2));
+    }
+}
